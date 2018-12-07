5013ff9b243775d13bd65fe713d2337a26abd198
DIANA BERENICE <42920464+DianaBereniceSR@users.noreply.github.com>
2018-12-07 15:48:36 -0800

    Add files via upload

--- RefugioAPI/mascota/urls.py
+++ RefugioAPI/mascota/urls.py
@@ -1,10 +1,14 @@
 from django.urls import path
 from mascota import views
+from django.contrib.auth.views import LoginView, logout_then_login
+from django.contrib.auth.decorators import login_required
 #from .models import Gato
 
 app_name="mascota"
 
 urlpatterns=[
-	path('',views.list_Gatos.as_view(),name="list_view"),
-	path('create/',views.createGato.as_view(),name="create_view"),
+	path('', login_required(views.list_Gatos.as_view()),name="list_view"),
+	path('create/', login_required(views.createGato.as_view()),name="create_view"),
+	path('accounts/login/',LoginView.as_view(template_name='Gato/login.html'),name="login_view"),
+	path('logout/',logout_then_login, name='logout'),
 ]
